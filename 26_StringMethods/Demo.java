26ba599322eda9ace1c00c8211155efda0a734f0
udit-070 <researchuditansu@gmail.com>
2026-04-29 18:27:20 +0530

    Added Some string methods

---
+++ 26_StringMethods/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        
+        char[] arr = {'U', 'd', 'i' , 't'};
+    
+        String s1 = new String(arr, 0, 3);
+        System.out.println(s1);
+    }
+}
+
+
