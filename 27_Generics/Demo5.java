f10fc915d8a7e0c18ae9d6ee070b65ce81c9773b
udit-070 <researchuditansu@gmail.com>
2026-05-03 18:58:06 +0530

    Between the chaos of Smeesters

--- 27_Generics/Demo5.java
+++ 27_Generics/Demo5.java
@@ -1,3 +1,14 @@
 public class Demo5 {
-    
+    // Now we want to know can we bound the generics like we can restrict what values T can take like? --> only numbers or only strings
+    public static void main(String[] args) {
+        Box<Integer> b1 = new Box<>();
+        b1.value = 5;
+        System.out.println(b1.value.getDouble());
+    }
 }
+
+class Box<T extends Number> { // This is known as Upper Bound.
+    T value;
+}
+
+// We know that by default T takes objects. But now i want to bound T to only Numbers. 
